c4f847424e61ae76ada2008142c43e0510787e6e
b-cancel <bryan.o.cancel@gmail.com>
2018-01-29 12:09:07 -0600

    filled in all distance calculation... soon to finish lerpValue caculation... ESSENTIAL commit

--- ColorKit/Assets/scripts/lerpHelper.cs
+++ ColorKit/Assets/scripts/lerpHelper.cs
@@ -43,35 +43,70 @@ public static class lerpHelper{
 
     public static float calcGuideDistance(guideDistance GD, float startValue, float currValue, float endValue)
     {
-        return 0;
+        //NOTE: guideDistance.other has no definition for anything but color
+
+        if (GD == guideDistance.distBetween_StartAndCurr)
+            return Mathf.Abs(startValue - currValue);
+        else if (GD == guideDistance.distBetween_StartAndEnd)
+            return Mathf.Abs(startValue - endValue);
+        else //guideDistance.distBetween_CurrAndEnd
+            return Mathf.Abs(currValue - endValue);
     }
 
     //-------------------------2 Dimensional
 
     public static float calcGuideDistance(guideDistance GD, Vector2 startVect2, Vector2 currVector2, Vector2 endVector2)
     {
-        return 0;
+        //NOTE: guideDistance.other has no definition for anything but color
+
+        if (GD == guideDistance.distBetween_StartAndCurr)
+            return Vector2.Distance(startVect2, currVector2);
+        else if (GD == guideDistance.distBetween_StartAndEnd)
+            return Vector2.Distance(startVect2, endVector2);
+        else //guideDistance.distBetween_CurrAndEnd
+            return Vector2.Distance(currVector2, endVector2);
     }
 
     //-------------------------3 Dimensional
 
     public static float calcGuideDistance(guideDistance GD, Vector3 startVect3, Vector3 currVector3, Vector3 endVector3)
     {
-        return 0;
+        //NOTE: guideDistance.other has no definition for anything but color
+
+        if (GD == guideDistance.distBetween_StartAndCurr)
+            return Vector3.Distance(startVect3, currVector3);
+        else if (GD == guideDistance.distBetween_StartAndEnd)
+            return Vector3.Distance(startVect3, endVector3);
+        else //guideDistance.distBetween_CurrAndEnd
+            return Vector3.Distance(currVector3, endVector3);
     }
 
     //-------------------------4 Dimensional
 
     public static float calcGuideDistance(guideDistance GD, Vector4 startVect4, Vector4 currVector4, Vector4 endVector4)
     {
-        return 0;
+        //NOTE: guideDistance.other has no definition for anything but color
+
+        if (GD == guideDistance.distBetween_StartAndCurr)
+            return Vector4.Distance(startVect4, currVector4);
+        else if (GD == guideDistance.distBetween_StartAndEnd)
+            return Vector4.Distance(startVect4, endVector4);
+        else //guideDistance.distBetween_CurrAndEnd
+            return Vector4.Distance(currVector4, endVector4);
     }
 
     //-------------------------BASE
 
     public static float calcGuideDistance(guideDistance GD, float[] startValues, float[] currValues, float[] endValues)
     {
-        return 0;
+        //NOTE: guideDistance.other has no definition for anything but color
+
+        if (GD == guideDistance.distBetween_StartAndCurr)
+            return euclideanDistance(startValues, currValues);
+        else if (GD == guideDistance.distBetween_StartAndEnd)
+            return euclideanDistance(startValues, endValues);
+        else //guideDistance.distBetween_CurrAndEnd
+            return euclideanDistance(currValues, endValues);
     }
 
     //-------------------------Colors
@@ -179,41 +214,37 @@ public static class lerpHelper{
 
     public static float calcLerpValue(colorSpace CS, Color startColor, Color currColor, Color endColor, float lerpVelocity_DperF) //this uses the calcLerpValue function for vector3s
     {
-        //calc vel
-        float velocity = 0;
 
-        //VEL = distance / framesToTravelGD;
-
-        //distance left to travel
-
-        float dist_C_2_E = 0;
+        //---calc distance left to travel
+        float distToFinish = 0;
         switch (CS)
         {
             case colorSpace.RGB:
-                dist_C_2_E = colorDistances.distBetweenColors(colorSpace.RGB, currColor, endColor);
+                distToFinish = colorDistances.distBetweenColors(colorSpace.RGB, currColor, endColor);
                 break;
             case colorSpace.RYB:
-                dist_C_2_E = colorDistances.distBetweenColors(colorSpace.RYB, currColor, endColor);
+                distToFinish = colorDistances.distBetweenColors(colorSpace.RYB, currColor, endColor);
                 break;
             default:
-                dist_C_2_E = colorDistances.distBetweenColors(colorSpace.CMYK, currColor, endColor);
+                distToFinish = colorDistances.distBetweenColors(colorSpace.CMYK, currColor, endColor);
                 break;
         }
 
         //---Calculate how many frames we have left to finish (based on our velocity)
-        float framesToFinsih_C_2_E = velocity * dist_C_2_E;
+        float framesToFinish = distToFinish / lerpVelocity_DperF;
 
-        //NOTE: I was previously using FloorToInt()... using Ceil is untested
-        framesToFinsih_C_2_E = (int)Mathf.CeilToInt(framesToFinsih_C_2_E);
+        //frames are integers... not floats sowe convert
+        //NOTE: this will overshoot our destination... BUT your result is clamped so we DONT return the overshot value
+        framesToFinish = (int)Mathf.CeilToInt(framesToFinish);
 
         //NOTE: as of now we know how many frames every distance will take... so now we use said data to lerp
 
         //NOTE: "distPerFrame" that took so long to calculate WILL NOT CHANGE because we are aiming for a LERP
         //so we travel the same distance every frame AS LONG AS OUR SETTINGS STAY THE SAME
-        float distPerFrame = dist_C_2_E / framesToFinsih_C_2_E;
+        float distPerFrame = distToFinish / framesToFinish;
 
         //TODO... The Implication is that we only need to run this function once  and then just run "the one line" below every frame
-        float lerpValue = Mathf.Clamp((distPerFrame / dist_C_2_E), 0, 1);
+        float lerpValue = Mathf.Clamp((distPerFrame / distToFinish), 0, 1);
 
         return lerpValue;
     }
